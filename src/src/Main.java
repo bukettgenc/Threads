81ddc3ed36413c095b156b0c1c854158779e4510
buketgenc <bktgnc06@gmail.com>
2023-10-16 23:55:01 +0300

    initial commit

---
+++ src/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+public class Main {
+//Threadler processlerin hafif halidir.
+//Bir programın en küçük bağımsız birimidir
+//Her java programı bir thread içerir
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
